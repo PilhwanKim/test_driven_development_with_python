d42147908e5ff1a331010f57d4bc27e52824d487
Pilhwan Kim <pilhwankim03@gmail.com>
2019-12-23 09:00:44 +0900

    rename ch dir2

--- test_driven_development_with_python/ch01_Getting_Django_Set_Up_Using_a_Functional_Test/01-01/functional_test.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'Django' in browser.title
--- test_driven_development_with_python/ch03_Testing_a_Simple_Home_Page_with_Unit_Tests/03-06/superlists/lists/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
