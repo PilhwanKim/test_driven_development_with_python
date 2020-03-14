d457bee710ccf42bc0535aa2cd43c75e7b695221
pilhwanKim <pilhwankim03@gmail.com>
2020-03-15 07:22:41 +0900

    11장 테스트 건너 뛰기

--- ch11/11-01/superlists/functional_tests/tests.py
+++ ch11/11-01/superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 import os
+from unittest import skip
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -120,18 +121,19 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10
         )
 
+    @skip
     def test_cannot_add_empty_list_items(self):
-        # Edith는 홈페이지로 이동하여 실수로 빈 목록 항목을 제출하려고 시도합니다.
-        # 빈 입력 상자에서 Enter 키를 누르십시오.
+        # 에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려고 한다.
+        # 입력 상자가 비어있는 상태에서 엔터키를 누른다.
 
-        # 홈페이지가 새로 고쳐지고 오류 메시지가 표시됩니다
-        # 해당 목록 항목은 비워 둘 수 없습니다
+        # 페이지가 새로 고침되고, 빈 아이템을 등록할 수 없다는 
+        # 에러 메시지가 표시된다
 
-        # 그녀는 항목에 대한 텍스트를 사용하여 다시 시도하며 이제 작동합니다
+        # 다른 아이템을 입력하고 이번에는 정상 처리된다
 
-        # 반대로 그녀는 이제 두 번째 빈 목록 항목을 제출하기로 결정합니다
+        # 그녀는 고의적으로 다시 빈 아이템을 등록한다.
 
-        # 그녀는 목록 페이지에서 비슷한 경고를받습니다.
+        # 리스트 페이지에 다시 에러 메시지가 표시된다.
 
-        # 그리고 그녀는 텍스트를 채워서 수정할 수 있습니다
+        # 아이템을 입력하면 정상 동작한다.
         self.fail('write me!')
